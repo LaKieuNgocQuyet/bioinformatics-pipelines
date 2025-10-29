735a921c8f6c5f02a6cfce44d4d8e3b923edb475
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-10-29 15:56:20 +0700

    Delete variant_calling.py

--- src/variant_calling/variant_calling.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import argparse
-
-parser = argparse.ArgumentParser(description="None")
-parser.add_argument("-I", "--input",required=True, nargs="+", type=str, help="None")
-parser.add_argument("-R", "--reference", required=True, type=str, help="None")
-parser.add_argument("-O", "--outdir", required=False, type=str, help="None")
-args = parser.parse_args()
