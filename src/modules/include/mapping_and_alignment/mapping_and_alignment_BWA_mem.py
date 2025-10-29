989d6758e7616efeaadf6259eda167a8c1afb5f2
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-10-29 17:29:41 +0700

    Update mapping_and_alignment_BWA_mem.py

--- src/modules/include/mapping_and_alignment/mapping_and_alignment_BWA_mem.py
+++ src/modules/include/mapping_and_alignment/mapping_and_alignment_BWA_mem.py
@@ -3,11 +3,12 @@ import subprocess
 def mapping_and_alignment_BWA_mem(FORWARD, REVERSE, REFERENCE, OUTDIR, SAM_FILE):
 
     short_read = f"""
-        /usr/bin/time -v \
-            bwa mem -t 8 -R "@RG\\tID:group1\\tLB:lib1\\tPL:illumina\\tPU:unit1\\tSM:sample1" \
-            {REFERENCE} \
-            {FORWARD} \
-            {REVERSE} \
+        /usr/bin/time -v -o {OUTDIR}/runtime.log \
+            bwa mem -t 8 \
+                -R "@RG\\tID:group1\\tLB:lib1\\tPL:illumina\\tPU:unit1\\tSM:sample1" \
+                {REFERENCE} \
+                {FORWARD} \
+                {REVERSE} \
             > {OUTDIR}/{SAM_FILE} \
         2>> {OUTDIR}/monitoring.log
     """
