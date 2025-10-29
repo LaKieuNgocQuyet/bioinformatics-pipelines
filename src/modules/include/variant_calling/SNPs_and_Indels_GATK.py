c6fd316ad27b1e35bd0acff6e7190fd826d6042f
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-10-29 17:17:46 +0700

    Update SNPs_and_Indels_GATK.py

--- src/modules/include/variant_calling/SNPs_and_Indels_GATK.py
+++ src/modules/include/variant_calling/SNPs_and_Indels_GATK.py
@@ -8,6 +8,7 @@ def SNPs_and_Indels_GATK(RECAL_BAM, REFERENCE, OUTDIR, SAMPLE_NAME):
                 -I {RECAL_BAM} \
                 -R {REFERENCE} \
                 -O {OUTDIR}/{SAMPLE_NAME}.raw.vcf \
+                -ERC GVCF
                 -L /home/lknq/hg19/S07604624_Regions.bed \                
         2>> {OUTDIR}/monitoring.log
     """
