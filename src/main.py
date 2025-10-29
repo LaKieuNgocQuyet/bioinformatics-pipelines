870268ef1069120c1f7a7a886254bcee827c172b
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-10-29 17:17:31 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import yaml
 import argparse
 import sys
+import subprocess
 sys.dont_write_bytecode = True
-from modules import header
+
+from modules.header import *
 
 parser = argparse.ArgumentParser(description="None")
 parser.add_argument("-I", "--input",required=True, type=str, help="None")
@@ -18,15 +20,26 @@ with open(f"{INPUT_YAML}", "r") as f:
     REFERENCE = input["reference"]["genome"]
     KNOWN_SITES = input["reference"]["known_site"]
     OUTDIR = input["output"]["directory"]
-
+# Group samples
 for sample in SAMPLES:
     ID = sample["id"]
     R1 = sample["read1"]
     R2 = sample["read2"]
 
+    FORWARD_AVERAGE_LENGTH = check_average_read_length(R1)
+    REVERSE_AVERAGE_LENGTH = check_average_read_length(R2)
+    AVERAGE_LENGTH = (FORWARD_AVERAGE_LENGTH + REVERSE_AVERAGE_LENGTH) / 2
+    
+    if AVERAGE_LENGTH >= 250:
+        READ_TYPE = "long"
+    else:
+        READ_TYPE = "short"
+
     SAMPLE_LIST[ID] = {
         "read1": R1,
-        "read2": R2
+        "read2": R2,
+        "read_type": READ_TYPE
     }
+print(SAMPLE_LIST)
 
 
