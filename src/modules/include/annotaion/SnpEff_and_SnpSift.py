b0f9a2204b1ed62c8049bae3559a17e8127c6480
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-10-30 01:13:04 +0700

    Update SnpEff_and_SnpSift.py

--- src/modules/include/annotaion/SnpEff_and_SnpSift.py
+++ src/modules/include/annotaion/SnpEff_and_SnpSift.py
@@ -1,1 +1,8 @@
+import subprocess
 
+def snpEff_annotation(VCF_INPUT, OUTDIR):
+    command = f"""
+        /usr/bin/time -v -o {OUTDIR}/runtime.log \
+            snpEff -lof GRCh37.p13 {VCF_INPUT} > {OUTDIR}/{vcf_output} \
+        2>> {OUTDIR}/monitoring.log
+    """
