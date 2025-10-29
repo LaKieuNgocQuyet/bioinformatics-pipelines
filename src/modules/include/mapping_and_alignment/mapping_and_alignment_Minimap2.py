dbc95975833f43937b892d0c99e5da1148fc7abe
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-10-30 01:13:22 +0700

    Create mapping_and_alignment_Minimap2.py

---
+++ src/modules/include/mapping_and_alignment/mapping_and_alignment_Minimap2.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def mapping_and_alignment_Minimap2(SAMPLE_ID, PLATFORM, FORWARD, REVERSE, REFERENCE, OUTDIR, SAMPLE_OUTDIR, SAM_FILE):
+
+    command = f"""
+        /usr/bin/time -v -o {OUTDIR}/runtime.log \
+            minimap2 -x -a \
+                {REFERENCE}.mmi \
+                -R "@RG\\tID:{SAMPLE_ID}\\tLB:lib1\\tPL:{PLATFORM}\\tPU:unit1\\tSM:{SAMPLE_ID}" \
+                {FORWARD} \
+                {REVERSE} \
+                > {SAMPLE_OUTDIR}/{SAM_FILE} \
+        2>> {OUTDIR}/monitoring.log
+    """
+    subprocess.run(command, shell=True, check=True)
