b79f2228cd524068d447ad86f496c08d08b6f542
Lã Kiều Ngọc Quyết <quyetlakn@gmail.com>
2025-10-30 17:19:10 +0700

    Update SNPs_and_Indels_GATK.py

--- src/modules/include/variant_calling/SNPs_and_Indels_GATK.py
+++ src/modules/include/variant_calling/SNPs_and_Indels_GATK.py
@@ -12,3 +12,14 @@ def SNPs_and_Indels_GATK(RECAL_BAM_FILE, REFERENCE, SAMPLE_OUTDIR, OUTDIR, RAW_G
         2>> {OUTDIR}/monitoring.log
     """
     subprocess.run(command, shell=True, check=True)
+
+def combine_gvcfs(GVCF_FILE_LIST, REFERENCE, OUTDIR):
+    command = f"""
+        /usr/bin/time -v -a -o {OUTDIR}/runtime.log \
+            gatk CombineGVCFs \
+                -R {REFERENCE} \
+                {GVCF_FILE_LIST} \
+                -O {OUTDIR}/variant_combined.g.vcf \
+        2>> {OUTDIR}/monitoring.log
+    """
+    subprocess.run(command, shell=True, check=True)
